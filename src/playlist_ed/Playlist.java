64dba8a66188f2efd79a02ceb42a7ca1ed2ab18a
Yeyson Cano Carbajo <Yeyson Cano Carbajo@192.168.1.6>
2022-06-13 19:32:00 -0500

    He modificado la interfaz de retirar

--- src/playlist_ed/Playlist.java
+++ src/playlist_ed/Playlist.java
@@ -4,7 +4,17 @@ public class Playlist {
     public void agregarCancion(Cancion nuevacancion){}
     public void eliminarCancion(int index){}
     public void ordenarXNombre(){
-    
+        public int[] ordenarInsercion(int[] array){
+        int aux;
+        for (int i = 1; i < array.length; i++) {
+            aux = array[i];
+            for (int j = i-1; j >=0 && array[j]>aux; j--) {
+                array[j+1]=array[j];
+                array[j]=aux;
+            }
+        }
+        return array;
+        }
     }
     public void ordenarXDuracion(){}
     public void ordenarXFecha(){}

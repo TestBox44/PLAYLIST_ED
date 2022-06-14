846fde372d9e65280adc9b758b00ba8b6c5cfce8
TestBox44 <iceglace12@hotmail.com>
2022-06-13 19:29:17 -0500

    Carga de la base del proyecto NETBEANS

---
+++ src/playlist_ed/Playlist.java
@@ -0,0 +1,11 @@
+package playlist_ed;
+public class Playlist {
+    Cancion[] listacanciones;
+    public void agregarCancion(Cancion nuevacancion){}
+    public void eliminarCancion(int index){}
+    public void ordenarXNombre(){
+    
+    }
+    public void ordenarXDuracion(){}
+    public void ordenarXFecha(){}
+}

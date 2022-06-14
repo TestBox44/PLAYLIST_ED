dbec53e326e2fab26575bf61764645d948437542
TestBox44 <iceglace12@hotmail.com>
2022-06-13 20:17:56 -0500

    avance de Playlist

--- src/playlist_ed/PLAYLIST_ED.java
+++ src/playlist_ed/PLAYLIST_ED.java
@@ -1,20 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package playlist_ed;
 
-/**
- *
- * @author USER
- */
+import java.util.Date;
+
 public class PLAYLIST_ED {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("AHORA SI?");
+        Playlist p1=new Playlist();
+        Date d1 = new Date("04/02/2010");
+        Cancion c1=new Cancion("Niña Bonita",3.0f,d1);
+        Date d2 = new Date("01/03/2005");
+        Cancion c2=new Cancion("Cholo soy",3.5f,d2);
+        Date d3 = new Date("10/11/2020");
+        Cancion c3=new Cancion("That's Life",4f,d3);
+        p1.agregarCancion(c1);
+        p1.agregarCancion(c2);
+        p1.agregarCancion(c3);
+        for (int i = 0; i < p1.listacanciones.length; i++) {
+            System.out.println(p1.listacanciones[i].getNombre());
+        }
+        p1.ordenarXNombre();
+        System.out.println("---------------------------------------");
+        for (int i = 0; i < p1.listacanciones.length; i++) {
+            System.out.println(p1.listacanciones[i].getNombre());
+        }
     }
     
 }
--- src/playlist_ed/Playlist.java
+++ src/playlist_ed/Playlist.java
@@ -1,21 +1,49 @@
 package playlist_ed;
+import java.util.ArrayList; 
 public class Playlist {
     Cancion[] listacanciones;
-    public void agregarCancion(Cancion nuevacancion){}
+
+    public Playlist() {
+        listacanciones=new Cancion[0];
+    }
+    
+    public void agregarCancion(Cancion nuevacancion){
+        Cancion[] nuevalistacanciones = new Cancion[listacanciones.length+1];
+        for (int i = 0; i < listacanciones.length; i++) {
+            nuevalistacanciones[i]=listacanciones[i];
+        }
+        nuevalistacanciones[listacanciones.length]=nuevacancion;
+        listacanciones=nuevalistacanciones;
+    }
     public void eliminarCancion(int index){}
     public void ordenarXNombre(){
-        public int[] ordenarInsercion(int[] array){
-        int aux;
-        for (int i = 1; i < array.length; i++) {
-            aux = array[i];
+        Cancion aux;
+        for (int i = 1; i < listacanciones.length; i++) {
+            aux = listacanciones[i];
+            for (int j = i-1; j >=0 && listacanciones[j].getNombre().compareTo(aux.getNombre())>0; j--) {
+                listacanciones[j+1]=listacanciones[j];
+                listacanciones[j]=aux;
+            }
+        }
+    }/*
+    public void ordenarXDuracion(){
+        Cancion aux;
+        for (int i = 1; i < listacanciones.length; i++) {
+            aux = listacanciones[i];
             for (int j = i-1; j >=0 && array[j]>aux; j--) {
                 array[j+1]=array[j];
                 array[j]=aux;
             }
         }
-        return array;
-        }
     }
-    public void ordenarXDuracion(){}
-    public void ordenarXFecha(){}
+    public void ordenarXFecha(){
+        Cancion aux;
+        for (int i = 1; i < listacanciones.length; i++) {
+            aux = listacanciones[i];
+            for (int j = i-1; j >=0 && array[j]>aux; j--) {
+                array[j+1]=array[j];
+                array[j]=aux;
+            }
+        }
+    }*/
 }
